00ab61ca349dbbdd84c6694c470acee3432a48cb
Valentine <vpierres@gmail.com>
2015-08-20 14:59:47 +0200

    second commit

--- Gemfile
+++ Gemfile
@@ -30,7 +30,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'unicorn'
 
 # Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
+gem 'capistrano-rails', group: :development
+
+# BOOTSTRAP
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+	// PARALAX 
+
+	var controller = new ScrollMagic.Controller();
+
+	var scene = new ScrollMagic.Scene({
+		triggerElement: "#second-section",
+		triggerHook: 0
+	})
+		.setClassToggle(".bloc-picto img", "movImg")
+		//.addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
+		.addTo(controller);
+
+
+	var scene = new ScrollMagic.Scene({
+		triggerElement: "#third-section",
+		triggerHook: 0
+	})
+		.setClassToggle(".left span", "mov")
+		//.addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
+		.addTo(controller);
+
+	var scene = new ScrollMagic.Scene({
+		triggerElement: "#third-section",
+		triggerHook: 0
+	})
+		.setClassToggle(".right span", "mov")
+		//.addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
+		.addTo(controller);
+});
